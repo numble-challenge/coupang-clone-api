a8baef26d1e08518e93eca14c05ebed8f0564776
numble-challenger <numble.challenge@gmail.com>
2022-09-02 18:47:56 +0900

    feat(api): add cart-item update,delete handler

---
+++ pages/api/cart-items/[id].ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { bearer, cors } from "@/lib";
+import { CartRepository } from "@/repositories";
+
+export default cors(async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const userId = bearer(req, "access");
+  const id = Number(req.query.id);
+
+  if (req.method === "PATCH") {
+    if (!req.body.quantity) {
+      res.status(400).send("Invalid quantity");
+    }
+
+    await CartRepository.update(userId, id, { quantity: req.body.quantity });
+  } else if (req.method === "DELETE") {
+    await CartRepository.delete(userId, id);
+  } else {
+    res.status(404).send(`Cannot ${req.method} /api/cart-items`);
+  }
+
+  res.status(200).json(true);
+});
